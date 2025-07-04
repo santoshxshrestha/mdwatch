e8539e8d8c54ec1255934b95b58a6bb20ac41eab
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-07-04 17:31:39 +0545

    added the dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::App;
+use actix_web::HttpServer;
+#[actix_web::main]
+async fn main() {
+    HttpServer::new(|| {
+        App::new()
+            .service(Files::new("/static", "./static"))
+            .service(hello)
+    })
 }
