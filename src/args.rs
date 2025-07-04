0de5ce9c3cc3e92a89f22df5da39a729a43e849f
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-07-04 18:19:53 +0545

    declared some args and started parsing it

---
+++ src/args.rs
@@ -0,0 +1,24 @@
+use clap;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[clap(
+    name = "mdserve",
+    author = "santoshxshrestha",
+    version,
+    about = "A CLI tool to live-preview Markdown files in your browser using a local web server"
+)]
+
+pub struct MdwatchArgs {
+    /// Path to the markdown file
+    pub file: String,
+
+    /// IP address to bind the server (default: 127.0.0.1)
+    #[clap(short, long, default_value = "127.0.0.1")]
+    pub ip: String,
+
+    /// Port number to serve on (default: 3000)
+    #[clap(short, long, default_value_t = 3000)]
+    pub port: u16,
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 #![allow(unused)]
+mod args;
 use actix_files::Files;
 use actix_web::App;
 use actix_web::HttpResponse;
 use actix_web::HttpServer;
 use actix_web::Responder;
 use actix_web::get;
+use args::MdwatchArgs;
 use askama::Template;
+use clap::Parser;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::sync::MutexGuard;
+use std::sync::atomic::AtomicU16;
 
 #[derive(Template)]
 #[template(path = "home.html")]
@@ -26,6 +33,12 @@ async fn home() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let args = MdwatchArgs::parse();
+
+    let file = Arc::new(Mutex::new(String::new()));
+    let port = Arc::new(AtomicU16::new(0));
+    let ip = Arc::new(Mutex::new(String::new()));
+
     HttpServer::new(move || {
         App::new()
             .service(Files::new("/static", "./static"))
