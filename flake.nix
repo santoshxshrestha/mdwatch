aaaed256a1c1bcf3184f94b3a0ef5ab4bbdf05e5
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-18 22:10:16 +0545

    update: flake

--- flake.nix
+++ flake.nix
@@ -1,16 +1,35 @@
 {
-  description = "Flake for mdwatch";
+  description = "A Nix-flake-based Rust development environment for mdwatch";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { nixpkgs, naersk, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ rustc cargo rustfmt openssl clippy ];
-        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      pkgs = nixpkgs.legacyPackages.${system};
+      naerskLib = pkgs.callPackage naersk { };
+    in
+    {
+      packages.${system}.default = naerskLib.buildPackage {
+        src = ./.;
+        nativeBuildInputs = [ pkgs.pkg-config ];
       };
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rustc
+          cargo
+          clippy
+          rustfmt
+          cargo-watch
+          rust-analyzer
+        ];
+        nativeBuildInputs = [ pkgs.pkg-config ];
+
+      };
+      formatter = pkgs.rustfmt;
     };
 }
