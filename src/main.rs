96dc68db72b7d2e99f182c3bdb058170be3e1db4
santoshxshrestha <santoshxshrestha@gmail.com>
2026-02-16 23:45:51 +0545

    fix: every files are not not excepted

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-mod template;
 use actix_web::web;
 use pulldown_cmark::Options;
 use std::fs;
 use std::path::Path;
 use std::sync::atomic::AtomicU64;
 use std::time::UNIX_EPOCH;
-use template::Home;
 mod args;
 use actix_web::App;
 use actix_web::HttpResponse;
@@ -19,17 +17,38 @@ use std::sync::Arc;
 use std::sync::Mutex;
 use std::sync::atomic::Ordering;
 
+#[derive(Template)]
+#[template(path = "main.html")]
+pub struct Mdwatch {
+    pub content: String,
+    pub last_modified: u64,
+    pub title: String,
+}
+
 #[get("/")]
 async fn home(
     file: web::Data<String>,
     last_modified: web::Data<Arc<AtomicU64>>,
 ) -> actix_web::Result<HttpResponse> {
-    let file_path = file.as_str();
-    let file = match Path::new(&file_path).file_name() {
+    let file_path = Path::new(file.as_str());
+
+    let file_name = match file_path.file_name() {
         Some(name) => name,
         None => {
             return Err(actix_web::error::ErrorInternalServerError(
                 "Failed to get file name",
+            ));
+        }
+    };
+
+    if let Some(extension) = file_path.extension() {
+        if extension != "md" {
+            eprintln!(
+                "Warning: Unsupported file type: .{}",
+                extension.to_string_lossy()
+            );
+            return Err(actix_web::error::ErrorInternalServerError(
+                "Unsupported file type. Please provide a markdown (.md) file.",
             ));
         }
     };
@@ -43,10 +62,10 @@ async fn home(
     pulldown_cmark::html::push_html(&mut html_output, parser);
     html_output = clean(&html_output);
 
-    let template = Home {
+    let template = Mdwatch {
         content: html_output,
         last_modified: last_modified.load(Ordering::SeqCst),
-        title: file.to_string_lossy().to_string(),
+        title: file_name.to_string_lossy().to_string(),
     };
 
     match template.render() {
