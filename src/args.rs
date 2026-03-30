bb15ac1a9e86a40923fcf2727d7c2bb814803681
vimlinuz <vimlinuz@gmail.com>
2026-03-30 14:35:18 +0545

    refacto: remove the redundent info in cli

--- src/args.rs
+++ src/args.rs
@@ -2,19 +2,20 @@ use clap::Parser;
 
 #[derive(Debug, Parser)]
 #[clap(
-    author = "santoshxshrestha",
+    author,
     version,
-    about = "A CLI tool to live-preview Markdown files in your browser using a local web server"
+    about = "A CLI tool to live-preview Markdown files in your browser using a local web server",
+    long_about = None,
 )]
 pub struct MdwatchArgs {
     /// Path to the markdown file
     pub file: String,
 
-    /// IP address to bind the server (default: 127.0.0.1)
+    /// IP address to bind the server
     #[clap(short, long, default_value = "127.0.0.1")]
     pub ip: String,
 
-    /// Port number to serve on (default: 3000)
+    /// Port number to serve on
     #[clap(short, long, default_value_t = 3000)]
     pub port: u16,
 }
